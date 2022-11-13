657f3aebc349c72a6502bdcff08dd3349bb1f2f6
Nihal Bakisgan <nihalkaratasbakisgan@gmail.com>
2022-11-13 22:21:31 +0100

    push

--- src/main/java/mentor_tasks/arackiralama/AracTalebi.java
+++ src/main/java/mentor_tasks/arackiralama/AracTalebi.java
@@ -17,5 +17,90 @@ public class AracTalebi {
     private String vitesTipi;
     private double gunlukUcret;
 
-
+public AracTalebi (String alinacakGun,String alinacakSehir,LocalDateTime alisSaati,LocalDateTime teslimSaati){
+    this.alinacakGun=alinacakGun;
+    this.alinacakSehir=alinacakSehir;
+    this.alisSaati=alisSaati;
+    this.teslimSaati=teslimSaati;
 }
+
+    public AracTalebi(String marka, String model, String yakitTipi, String vitesTipi, double gunlukUcret) {
+        this.marka = marka;
+        this.model = model;
+        this.yakitTipi = yakitTipi;
+        this.vitesTipi = vitesTipi;
+        this.gunlukUcret = gunlukUcret;
+    }
+
+    public String getAlinacakGun() {
+        return alinacakGun;
+    }
+
+    public void setAlinacakGun(String alinacakGun) {
+        this.alinacakGun = alinacakGun;
+    }
+
+    public String getAlinacakSehir() {
+        return alinacakSehir;
+    }
+
+    public void setAlinacakSehir(String alinacakSehir) {
+        this.alinacakSehir = alinacakSehir;
+    }
+
+    public LocalDateTime getAlisSaati() {
+        return alisSaati;
+    }
+
+    public void setAlisSaati(LocalDateTime alisSaati) {
+        this.alisSaati = alisSaati;
+    }
+
+    public LocalDateTime getTeslimSaati() {
+        return teslimSaati;
+    }
+
+    public void setTeslimSaati(LocalDateTime teslimSaati) {
+        this.teslimSaati = teslimSaati;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYakitTipi() {
+        return yakitTipi;
+    }
+
+    public void setYakitTipi(String yakitTipi) {
+        this.yakitTipi = yakitTipi;
+    }
+
+    public String getVitesTipi() {
+        return vitesTipi;
+    }
+
+    public void setVitesTipi(String vitesTipi) {
+        this.vitesTipi = vitesTipi;
+    }
+
+    public double getGunlukUcret() {
+        return gunlukUcret;
+    }
+
+    public void setGunlukUcret(double gunlukUcret) {
+        this.gunlukUcret = gunlukUcret;
+    }
+}//class
--- src/main/java/mentor_tasks/arackiralama/MusteriVeAracBilgileri.java
+++ src/main/java/mentor_tasks/arackiralama/MusteriVeAracBilgileri.java
@@ -1,4 +1,10 @@
 package mentor_tasks.arackiralama;
 
+import java.time.LocalDateTime;
+
 public class MusteriVeAracBilgileri {
+
+    public static void aracTalep(String alinnSehir, String teslmSehir, String alinackGun,String teslmGunu,LocalDateTime teslmSaati){
+
+    }
 }
