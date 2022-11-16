ecea26dcd79f0765c64ab094da3f420aeb8efcd8
Nihal Bakisgan <nihalkaratasbakisgan@gmail.com>
2022-11-17 00:08:51 +0100

    mittwoch

---
+++ src/main/java/mentor_tasks/Task_05.java
@@ -0,0 +1,16 @@
+package mentor_tasks;
+
+import java.util.HashSet;
+
+public class Task_05 {
+    /*
+    // Verilen bir String'deki harfleri ve harflerin kacar kez kullanildigini
+        // return eden bir method yaziniz
+        //  Ornek : Input : Zeynep Hoca bir tane   output : { =3, a=3, b=1, r=1, C=1, c=1, t=1, e=1, H=1, i=1, n=2, o=1}
+        String input="Zeynep Hoca bir tane";
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
